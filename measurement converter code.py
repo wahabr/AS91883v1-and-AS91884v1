a4f4ece29008401f014e6a900a8084335753e85e
wahabr <wahabr@stu.otc.school.nz>
2021-09-02 09:36:33 +1200

    tested and made alterations to code

--- measurement converter code.py
+++ measurement converter code.py
@@ -6,10 +6,10 @@ length_value_list = [1, 1000, 0.01, 0.001, 1609.35, 0.9144, 0.3048, 0.024]
 
 #Function for converting temperature
 def convert_temp(temp_list):
-    unit_temp = input("What unit would you like to convert from?")
+    unit_temp = input("What unit would you like to convert from?").strip().lower()
     if unit_temp in temp_list:
         num_temp = float(input("How many degrees {} would you like to convert?".format(unit_temp)))
-        des_unit_temp = str(input("What unit would you like to convert to?"))
+        des_unit_temp = str(input("What unit would you like to convert to?")).strip().lower()
         if des_unit_temp in temp_list:
             if unit_temp == "celsius" and des_unit_temp == "fahrenheit":
                 ans_temp = 1.8 * num_temp + 32
@@ -33,10 +33,10 @@ def convert_temp(temp_list):
 
 #Function for converting weight
 def convert_weight(weight_list, weight_value_list):
-    unit_weight = input("What unit would you like to convert from?")
+    unit_weight = input("What unit would you like to convert from?").strip().lower()
     if unit_weight in weight_list:
         num_weight = input("How many {} would you like to convert?".format(unit_weight))
-        des_unit_weight = input("What unit would you like to convert to?")
+        des_unit_weight = input("What unit would you like to convert to?").strip().lower()
         if des_unit_weight in weight_list:
             pos_weight = weight_list.index(unit_weight)
             weight_value = weight_value_list(pos_weight)
@@ -50,11 +50,11 @@ def convert_weight(weight_list, weight_value_list):
         print("Please enter a valid weight unit")
 
 #Function for converting length
-def convert_length(length_list,length_value_list):
-    unit_length = input("What unit would you like to convert from?")
+def convert_length(length_list, length_value_list):
+    unit_length = input("What unit would you like to convert from?").strip().lower()
     if unit_length in length_list:
         num_length = input("How many {} would you like to convert?".format(unit_length))
-        des_unit_length = input("What unit would you like to convert to?")
+        des_unit_length = input("What unit would you like to convert to?").strip().lower()
         if des_unit_length in weight_list:
             pos_length = length_list.index(unit_length)
             length_value = length_value_list(pos_length)
@@ -68,11 +68,11 @@ def convert_length(length_list,length_value_list):
         print("Please enter a valid length unit")
 
 
-convert = input("Would you like to convert a measurement")
+convert = input("Would you like to convert a measurement").strip().lower()
 if convert == "yes":
-    unit = input("Would you like to convert length, weight, or temperature?")
+    unit = input("Would you like to convert length, weight, or temperature?").strip().lower()
     if unit == "weight":
-        convert_weight(weight_list,weight_value_list)
+        convert_weight(weight_list, weight_value_list)
     elif unit == "length":
         convert_length(length_list, length_value_list)
     elif unit == "temperature":
