9f1e5aa164881413d7981b39805c0702bce8aa04
wahabr <wahabr@stu.otc.school.nz>
2021-09-08 08:53:24 +1200

    tested and made alterations to code

--- measurement converter code.py
+++ measurement converter code.py
@@ -1,3 +1,5 @@
+import numbers
+
 weight_value_list = [1, 1000, 0.001, 1000000, 1016046.08, 907184, 28.3495]
 weight_list = ["grams", "kilograms", "milligrams", "metric tons", "long tons", "short tons", "ounces"]
 temp_list = ["celsius", "fahrenheit", "kelvin"]
@@ -10,32 +12,36 @@ def convert_temp(temp_list):
     unit_temp = input("What unit would you like to convert from?").strip().lower()
     if unit_temp in temp_list:
         num_temp = float(input("How many degrees {} would you like to convert?".format(unit_temp)))
-        print("These are your options: {}".format(temp_list))
-        des_unit_temp = str(input("What unit would you like to convert to?")).strip().lower()
-        if des_unit_temp in temp_list:
-            if unit_temp == "celsius" and des_unit_temp == "fahrenheit":
-                ans_temp = 1.8 * num_temp + 32
-                print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
-            elif unit_temp == "celsius" and des_unit_temp == "kelvin":
-                ans_temp = num_temp + 273.15
-                print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
-            elif unit_temp == "fahrenheit" and des_unit_temp == "celsius":
-                ans_temp = (num_temp - 32) / 1.8
-                print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
-            elif unit_temp == "fahrenheit" and des_unit_temp =="kelvin":
-                ans_temp = (num_temp - 32) / 1.8 + 273.15
-                print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
-            elif unit_temp == "kelvin" and des_unit_temp == "celsius":
-                ans_temp = num_temp - 273.15
-                print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
-            elif unit_temp == "kelvin" and des_unit_temp == "fahrenheit":
-                ans_temp = (num_temp - 273.15) * 1.8 + 32
-                print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
-            else:
-                print("Please enter two different valid temperature units")
+        if num_temp not in numbers:
+            print("Please enter a valid numerical response")
+            num_temp = float(input("How many degrees {} would you like to convert?".format(unit_temp)))
         else:
-            print("Please enter a valid temperature unit")
+            print("These are your options: {}".format(temp_list))
             des_unit_temp = str(input("What unit would you like to convert to?")).strip().lower()
+            if des_unit_temp in temp_list:
+                if unit_temp == "celsius" and des_unit_temp == "fahrenheit":
+                    ans_temp = 1.8 * num_temp + 32
+                    print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
+                elif unit_temp == "celsius" and des_unit_temp == "kelvin":
+                    ans_temp = num_temp + 273.15
+                    print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
+                elif unit_temp == "fahrenheit" and des_unit_temp == "celsius":
+                    ans_temp = (num_temp - 32) / 1.8
+                    print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
+                elif unit_temp == "fahrenheit" and des_unit_temp =="kelvin":
+                    ans_temp = (num_temp - 32) / 1.8 + 273.15
+                    print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
+                elif unit_temp == "kelvin" and des_unit_temp == "celsius":
+                    ans_temp = num_temp - 273.15
+                    print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
+                elif unit_temp == "kelvin" and des_unit_temp == "fahrenheit":
+                    ans_temp = (num_temp - 273.15) * 1.8 + 32
+                    print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
+                else:
+                    print("Please enter two different valid temperature units")
+            else:
+                print("Please enter a valid temperature unit")
+                des_unit_temp = str(input("What unit would you like to convert to?")).strip().lower()
     else:
         print("Please enter a valid temperature unit")
         unit_temp = input("What unit would you like to convert from?").strip().lower()
