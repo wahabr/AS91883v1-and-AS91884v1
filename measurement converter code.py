6fd8d19bebca50d42a2e8e2cc47d9fc9e9980242
wahabr <wahabr@stu.otc.school.nz>
2021-09-14 15:03:47 +1200

    created and implemented function to test if input is a number

--- measurement converter code.py
+++ measurement converter code.py
@@ -5,32 +5,45 @@ length_list = ["metres", "kilometres", "centimetres", "millimetres", "miles", "y
 length_value_list = [1, 1000, 0.01, 0.001, 1609.35, 0.9144, 0.3048, 0.024]
 
 
+#Function for testing an integer
+def test_float(num_unit):
+    try:
+        float(num_unit)
+        return True
+    except ValueError:
+        return False
+
 #Function for converting temperature
 def convert_temp(temp_list):
     print("These are your options: {}".format(temp_list))
     unit_temp = input("What unit would you like to convert from?").strip().lower()
     if unit_temp in temp_list:
-        num_temp = float(input("How many degrees {} would you like to convert?".format(unit_temp)))
+        a = False
+        while a is False:
+            num_temp = input("How many degrees {} would you like to convert?".format(unit_temp))
+            a = test_float(num_temp)
+            if a is False:
+                print("Please enter a valid numerical response")
         print("These are your options: {}".format(temp_list))
         des_unit_temp = str(input("What unit would you like to convert to?")).strip().lower()
         if des_unit_temp in temp_list:
             if unit_temp == "celsius" and des_unit_temp == "fahrenheit":
-                ans_temp = 1.8 * num_temp + 32
+                ans_temp = 1.8 * float(num_temp) + 32
                 print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
             elif unit_temp == "celsius" and des_unit_temp == "kelvin":
-                ans_temp = num_temp + 273.15
+                ans_temp = float(num_temp) + 273.15
                 print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
             elif unit_temp == "fahrenheit" and des_unit_temp == "celsius":
-                ans_temp = (num_temp - 32) / 1.8
+                ans_temp = (float(num_temp) - 32) / 1.8
                 print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
             elif unit_temp == "fahrenheit" and des_unit_temp == "kelvin":
-                ans_temp = (num_temp - 32) / 1.8 + 273.15
+                ans_temp = (float(num_temp) - 32) / 1.8 + 273.15
                 print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
             elif unit_temp == "kelvin" and des_unit_temp == "celsius":
-                ans_temp = num_temp - 273.15
+                ans_temp = float(num_temp) - 273.15
                 print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
             elif unit_temp == "kelvin" and des_unit_temp == "fahrenheit":
-                ans_temp = (num_temp - 273.15) * 1.8 + 32
+                ans_temp = (float(num_temp) - 273.15) * 1.8 + 32
                 print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_temp, des_unit_temp, num_temp, unit_temp, ans_temp, des_unit_temp))
             else:
                 print("Please enter two different valid temperature units")
@@ -45,16 +58,21 @@ def convert_weight(weight_list, weight_value_list):
     print("These are your options: {}".format(weight_list))
     unit_weight = input("What unit would you like to convert from?").strip().lower()
     if unit_weight in weight_list:
-        num_weight = input("How many {} would you like to convert?".format(unit_weight))
+        a = False
+        while a is False:
+            num_weight = input("How many degrees {} would you like to convert?".format(unit_weight))
+            a = test_float(num_weight)
+            if a is False:
+                print("Please enter a valid numerical response")
         print("These are your options: {}".format(weight_list))
         des_unit_weight = input("What unit would you like to convert to?").strip().lower()
         if des_unit_weight in weight_list:
             pos_weight = weight_list.index(unit_weight)
             weight_value = weight_value_list[pos_weight]
-            value_in_grams = num_weight * weight_value
+            value_in_grams = float(num_weight) * weight_value
             pos_des_weight = weight_list.index(des_unit_weight)
             des_weight_value = weight_value_list[pos_des_weight]
-            ans_weight = des_weight_value * num_weight
+            ans_weight = des_weight_value * value_in_grams
             print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_weight, des_unit_weight, num_weight, unit_weight, ans_weight, des_unit_weight))
         else:
             print("Please enter a valid weight unit")
@@ -67,16 +85,21 @@ def convert_length(length_list, length_value_list):
     print("These are your options: {}".format(length_list))
     unit_length = input("What unit would you like to convert from?").strip().lower()
     if unit_length in length_list:
-        num_length = input("How many {} would you like to convert?".format(unit_length))
+        a = False
+        while a is False:
+            num_length = input("How many degrees {} would you like to convert?".format(unit_length))
+            a = test_float(num_length)
+            if a is False:
+                print("Please enter a valid numerical response")
         print("These are your options: {}".format(length_list))
         des_unit_length = input("What unit would you like to convert to?").strip().lower()
         if des_unit_length in length_list:
             pos_length = length_list.index(unit_length)
             length_value = length_value_list[pos_length]
-            value_in_metres = num_length * length_value
+            value_in_metres = float(num_length) * length_value
             pos_des_length = length_list.index(des_unit_length)
             des_length_value = length_value_list[pos_des_length]
-            ans_length = des_length_value * num_length
+            ans_length = des_length_value * value_in_metres
             print("Original Measurement Units:{}\nConverted Measurement Units:{}\nOriginal Measurement:{} {}\nConverted Measurement:{} {}".format(unit_length, des_unit_length, num_length, unit_length, ans_length, des_unit_length))
         else:
             print("Please enter a valid length unit")
